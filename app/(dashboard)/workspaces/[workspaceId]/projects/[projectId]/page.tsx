65be04be6cfad13ab398207cb4c1296e466e118e
dien96 <didinrudini12rplb@gmail.com>
2025-12-04 07:40:44 +0700

    feat: add TaskViewSwitcher component to project details page; update projectId page to include task view options

--- app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button";
 import { getCurrent } from "@/features/auth/queries";
 import { getProject } from "@/features/projects/queries";
 import ProjectAvatar from "@/features/projects/components/project-avatar";
+import { TaskViewSwitcher } from "@/features/tasks/components/task-view-switcher";
 
 interface ProjectIdPageProps {
   params: Promise<{ projectId: string }>;
@@ -46,6 +47,7 @@ export default async function ProjectIdPage({ params }: ProjectIdPageProps) {
           </Button>
         </div>
       </div>
+      <TaskViewSwitcher />
     </div>
   );
 }
