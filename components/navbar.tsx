565a1df67f14ae6d726e38957a148f914786c52b
j2dien <129026502+j2dien@users.noreply.github.com>
2025-12-08 10:34:13 +0700

    fix: adjust navbar padding for improved layout consistency

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,7 @@ import { MobileSidebar } from "./mobile-sidebar";
 
 export function Navbar() {
   return (
-    <nav className="pt-4 px-6 flex items-center justify-between">
+    <nav className="px-6 flex items-center justify-between">
       <div className="flex-col hidden lg:flex">
         <h1 className="text-2xl font-semibold">Home</h1>
         <p className="text-muted-foreground">
